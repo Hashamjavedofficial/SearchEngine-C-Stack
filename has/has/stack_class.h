ab2d20e43c563c40aec1d1ee2d29169b8fc169af
Hasham Javed <hasham.hasham1@gmail.com>
2019-12-10 21:50:59 +0500

    Stack header setup

---
+++ has/has/stack_class.h
@@ -0,0 +1,15 @@
+#ifndef _STACK_CLASS_
+#define _STACK_CLASS_
+#include"struct_class.h"
+
+class stack87{
+public:
+	void push1(int n);
+	int pop1();
+	int top1();
+	void empty1();
+	int size1();
+	void design();
+};
+
+#endif
--- new.cpp
+++ new.cpp
@@ -96,36 +96,7 @@ stack<int> search(info in){
 	return s;
 }
 
-result setresult(stack<int>s){	
-	int sizeg = s.size(); int forpop;	result h1; int a,b,c,d,e,f,g,h;	a=b=c=d=e=f=g=h=0;
-	for (int i = 0; i < sizeg; i++){
-		forpop = s.top();
-		if (forpop == 1){
-			a++;	h1.arrset[1] = 1;
-		}
-		else if (forpop == 2){
-			b++;	h1.arrset[2] = 1;
-		}
-		else if (forpop == 3){
-			c++;	h1.arrset[3] = 1;
-		}
-		else if (forpop == 4){
-			d++;	h1.arrset[4] = 1;
-		}
-		else if (forpop == 5){
-			e++;	h1.arrset[5] = 1;
-		}
-		else if (forpop == 6){
-			f++;	h1.arrset[6] = 1;
-		}
-		else if (forpop == 7){
-			g++;	h1.arrset[7] = 1;
-		}
-		else if (forpop == 8){
-			h++;	h1.arrset[8] = 1;
-		}
-		s.pop();
-	}
+
 	h1.arrresult[1] = a; h1.arrresult[2] = b; h1.arrresult[3] = c; h1.arrresult[4] = d; h1.arrresult[5] = e;
 	h1.arrresult[6] = f; h1.arrresult[7] = g; h1.arrresult[8] = h;
 	return h1;
