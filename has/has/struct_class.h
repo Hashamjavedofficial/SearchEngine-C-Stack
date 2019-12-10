89e697abb4b26cb3921073c3287fa8c9dacca214
Hasham Javed <hasham.hasham1@gmail.com>
2019-12-10 21:52:37 +0500

    structures are implemented correctly

---
+++ has/has/struct_class.h
@@ -0,0 +1,16 @@
+#ifndef _STRUCT_CLASS_
+#define _STRUCT_CLASS_
+#include<iostream>
+#include<string>
+using namespace std;
+struct hasham
+{
+	char str[100];
+	int temp;
+	std::string word;
+	int a1, a2, a3, a4, a5, a6, a7, a8;
+	int arrset[9];
+	int arrresult[9];
+
+};
+#endif
